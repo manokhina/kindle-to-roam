03fb293f62e12adcdaeaf77cbfadf4c501102d44
Anastasiia Manokhina <amanokhina@google.com>
2020-12-29 21:03:59 +0300

    Fix duplicates in the page ending

--- kindle-to-roam/generate_roam.py
+++ kindle-to-roam/generate_roam.py
@@ -7,24 +7,25 @@ from collections import defaultdict
 
 class RoamGenerator:
 
-    def __init__(self, markdown_path, clippings_path, header_file):
+    def __init__(self, markdown_path, clippings_path, header_path):
         """
         :param markdown_path: str, path for saving md files
         :param clippings_path: str, path for downloading the clippings file
+        :param header_path: str, path for custom page header
         """
         self.clippings = ""
-        self.page_header = self.read_header(header_file)
+        self.page_header = self.read_header(header_path)
         self.clippings_list = list()
         self.diff = defaultdict(list)
         self.markdown_path = markdown_path
         self.existing_database = self.load_database()
         self.clippings_path = clippings_path
 
-    def read_header(self, header_file):
+    def read_header(self, header_path):
         """
         Reads header template for the page.
         """
-        with open(header_file, 'r') as f:
+        with open(header_path, 'r') as f:
             page_header = f.read()
         return page_header
 
@@ -80,11 +81,8 @@ class RoamGenerator:
 
                 self.existing_database = self.update_db(self.existing_database, page_name, text)
 
-                if page_name not in self.diff:
+                if page_name not in self.diff or text not in self.diff[page_name]:
                     self.diff = self.update_db(self.diff, page_name, text)
-
-                elif text not in self.diff[page_name]:
-                    self.diff[page_name].append(f"- {text}\n")
 
     @staticmethod
     def update_db(db, page_name, text):
@@ -95,7 +93,7 @@ class RoamGenerator:
         :param text: str, page text
         :return: db: dict, updated database
         """
-        previous_chunk = db[page_name][-1].strip("\n") if db[page_name] else ""
+        previous_chunk = db[page_name][-1].strip("\n").strip("- ") if db[page_name] else ""
 
         if previous_chunk.endswith(text):
             return db
@@ -167,8 +165,8 @@ if __name__ == "__main__":
                         help='clippings path')
     parser.add_argument('--markdown_path', type=str, nargs='?', default="markdown",
                         help='path for saving markdown files')
-    parser.add_argument('--header_file', type=str, nargs='?', default="header.txt",
+    parser.add_argument('--header_path', type=str, nargs='?', default="header.txt",
                         help='path to custom header file')
     args = parser.parse_args()
-    generator = RoamGenerator(args.markdown_path, args.clippings_path, args.header_file)
+    generator = RoamGenerator(args.markdown_path, args.clippings_path, args.header_path)
     generator.run()
